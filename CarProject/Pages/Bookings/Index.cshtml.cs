dc9c5edff6972495f7857cb20ea345c674931412
eempc <evc484@gmail.com>
2019-12-19 10:11:57 +0000

    minor title changes

--- CarProject/Pages/Bookings/Index.cshtml.cs
+++ CarProject/Pages/Bookings/Index.cshtml.cs
@@ -1,17 +1,17 @@
 ﻿using CarProject.Data;
 using CarProject.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Linq;
 using System.Security.Claims;
-using System;
+using System.Threading.Tasks;
 
 namespace CarProject {
+    [Authorize]
     public class BookingIndexModel : PageModel {
         private readonly CarProjectContext _context;
-        private string userId;
+        //private string userId;
 
         public BookingIndexModel(CarProjectContext context) {
             _context = context;
@@ -20,7 +20,7 @@ namespace CarProject {
         public IList<Booking> Booking { get; set; }
 
         public async Task OnGetAsync() {
-            userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            //userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             Booking = await _context.Booking
                 //.Where(b => b.OwnerId == userId)
                 .Include(b => b.User)
