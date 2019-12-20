540b92c55f8a398d8a45724dbc4e497393a900be
eempc <evc484@gmail.com>
2019-12-20 10:17:49 +0000

    new page vehicle range

---
+++ CarProject/Pages/VehicleRange.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarProject
+{
+    public class VehicleRangeModel : PageModel
+    {
+
+        [BindProperty]
+        public IList<Vehicle> Vehicles { get; set; }
+
+        public VehicleRangeModel() {
+
+        }
+
+        
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
