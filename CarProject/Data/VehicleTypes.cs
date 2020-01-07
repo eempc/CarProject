58c102e0f018010754c0fbcfb6053448d0587832
eempc <evc484@gmail.com>
2020-01-07 10:42:54 +0000

    added more cars

--- CarProject/Data/VehicleTypes.cs
+++ CarProject/Data/VehicleTypes.cs
@@ -10,7 +10,10 @@ namespace CarProject.Data {
             List<VehicleInfo> list = new List<VehicleInfo> {
                 new VehicleInfo() { Make = "Audi", Model = "A6", Description = "Lorum ipsum car" },
                 new VehicleInfo() { Make = "Audi", Model = "A3", Description = "Lorum ipsum car" },
-                new VehicleInfo() { Make = "Audi", Model = "A2", Description = "Lorum ipsum car" }
+                new VehicleInfo() { Make = "Audi", Model = "A1", Description = "Lorum ipsum car" },
+                new VehicleInfo() { Make = "Ford", Model = "Mondeo", Description = "Lorum ipsum car" },
+                new VehicleInfo() { Make = "Honda", Model = "Civic", Description = "Lorum ipsum car" },
+                new VehicleInfo() { Make = "Nissan", Model = "Leaf", Description = "Lorum ipsum car" },
             };
             return list;
         }
