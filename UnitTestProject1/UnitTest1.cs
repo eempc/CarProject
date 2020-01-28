2daae8c04bd258d210c900274b82aaf721f1c2f9
eempc <evc484@gmail.com>
2020-01-28 08:58:22 +0000

    Register confirm page update

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject1 {
     [TestClass]
     public class UnitTest1 {
+        // Unit test of the vehicle model, test the get image file name
         [TestMethod]
         public void TestMethod1() {
         }
