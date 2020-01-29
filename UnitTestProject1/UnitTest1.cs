bd4720026f92326abc2bb8160f07b243be411ca1
eempc <evc484@gmail.com>
2020-01-29 09:06:22 +0000

    vehicle moved to class

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -5,20 +5,20 @@ using System;
 namespace UnitTestProject1 {
     [TestClass]
     public class UnitTest1 {
+        Vehicle testVehicle = new Vehicle {
+            VehicleId = new Guid(),
+            RegistrationMark = "AA11AAA",
+            VehicleType = VehicleType.Car,
+            Make = "Audi",
+            Model = "A3",
+            Size = Size.Medium,
+            Seats = 5,
+            Rate = 12.50M
+        };
+
         // Unit test of the vehicle model, test the get image file name
         [TestMethod]
         public void VehicleMakeModelToGetImageFile() {
-            Vehicle testVehicle = new Vehicle {
-                VehicleId = new Guid(),
-                RegistrationMark = "AA11AAA",
-                VehicleType = VehicleType.Car,
-                Make = "Audi",
-                Model = "A3",
-                Size = Size.Medium,
-                Seats = 5,
-                Rate = 12.50M
-            };
-
             string expectedResult = "audia3.png";
             string actualResult = testVehicle.ImageFile;
 
