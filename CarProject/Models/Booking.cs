bee919e08e89e96951eca87d69e2462ffbc4dd72
eempc <evc484@gmail.com>
2020-01-29 08:21:59 +0000

    Booking headings

--- CarProject/Models/Booking.cs
+++ CarProject/Models/Booking.cs
@@ -5,23 +5,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarProject.Models {
     public class Booking : IBooking {
-        [Required, Key]
+        [Required, Key, Display(Name = "Booking no.")]
         public int BookingId { get; set; } // Standard int
 
-        [Required, DataType(DataType.DateTime)]
+        [Required, DataType(DataType.DateTime), Display(Name = "Order date")]
         public DateTime DateCreated { get; set; } // DateTime this entry was added to DB
 
         [Required, ForeignKey("User")]
         public string OwnerId { get; set; } // Which user booked the car
         public CarProjectUser User { get; set; }
 
-        [Required, DataType(DataType.Date)]
+        [Required, DataType(DataType.Date), Display(Name = "Booking start date")]
         public DateTime BookingStartDateTime { get; set; }
 
-        [Required, DataType(DataType.Date)]
+        [Required, DataType(DataType.Date), Display(Name = "Booking end date")]
         public DateTime BookingEndDateTime { get; set; }
 
-        [Required, DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)"), Range(0, Double.PositiveInfinity)]
+        [Required, DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)"), Range(0, Double.PositiveInfinity), Display(Name = "Amount")]
         public decimal PricePaid { get; set; }
 
         public bool PaymentConfirmed { get; set; }
