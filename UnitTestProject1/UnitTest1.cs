90485c18e0621ddb909557742cbb32df657923a1
eempc <evc484@gmail.com>
2020-01-29 08:32:43 +0000

    Vehicle.ImageFile test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,11 +1,28 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarProject.Models;
+using System;
 
 namespace UnitTestProject1 {
     [TestClass]
     public class UnitTest1 {
         // Unit test of the vehicle model, test the get image file name
         [TestMethod]
-        public void TestMethod1() {
+        public void VehicleMakeModelToGetImageFile() {
+            Vehicle testVehicle = new Vehicle {
+                VehicleId = new Guid(),
+                RegistrationMark = "AA11AAA",
+                VehicleType = VehicleType.Car,
+                Make = "Audi",
+                Model = "A3",
+                Size = Size.Medium,
+                Seats = 5,
+                Rate = 12.50M
+            };
+
+            string expectedResult = "audia3.png";
+            string actualResult = testVehicle.ImageFile;
+
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
