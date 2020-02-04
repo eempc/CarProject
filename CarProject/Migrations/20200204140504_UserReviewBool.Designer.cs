72cad9c3a20909483f2bb17c21dd840001f9f0cd
eempc <evc484@gmail.com>
2020-02-04 14:06:36 +0000

    added Approved Bool to UserReview model

--- CarProject/Migrations/20200204140504_UserReviewBool.Designer.cs
+++ CarProject/Migrations/20200204140504_UserReviewBool.Designer.cs
@@ -4,14 +4,16 @@ using CarProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarProject.Migrations
 {
     [DbContext(typeof(CarProjectContext))]
-    partial class CarProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20200204140504_UserReviewBool")]
+    partial class UserReviewBool
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarProject/Migrations/20200204140504_UserReviewBool.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarProject.Migrations
+{
+    public partial class UserReviewBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Approved",
+                table: "UserReview",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "UserReview");
+        }
+    }
+}
