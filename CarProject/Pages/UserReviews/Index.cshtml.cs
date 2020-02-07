bf21dfe1c3a5c811a52f1d83c282955b0e8e82d8
eempc <evc484@gmail.com>
2020-02-07 08:26:14 +0000

    cleaned up

--- CarProject/Pages/UserReviews/Index.cshtml.cs
+++ CarProject/Pages/UserReviews/Index.cshtml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using CarProject.Data;
+using CarProject.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using CarProject.Data;
-using CarProject.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace CarProject
-{
-    public class UserReviewIndexModel : PageModel
-    {
-        private readonly CarProject.Data.CarProjectContext _context;
+namespace CarProject {
+    public class UserReviewIndexModel : PageModel {
+        private readonly CarProjectContext _context;
 
-        public UserReviewIndexModel(CarProject.Data.CarProjectContext context)
-        {
+        public UserReviewIndexModel(CarProjectContext context) {
             _context = context;
         }
 
-        public IList<UserReview> UserReview { get;set; }
+        public IList<UserReview> UserReview { get; set; }
 
-        public async Task OnGetAsync()
-        {
+        public async Task OnGetAsync() {
             UserReview = await _context.UserReview
                 .Include(u => u.User).ToListAsync();
         }
