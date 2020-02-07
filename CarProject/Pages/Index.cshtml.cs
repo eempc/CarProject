92e56a642ba0c8ec8275637de1508383126f60d0
eempc <evc484@gmail.com>
2020-02-07 08:26:37 +0000

    Basic EF load UserReviews

--- CarProject/Pages/Index.cshtml.cs
+++ CarProject/Pages/Index.cshtml.cs
@@ -1,12 +1,17 @@
-﻿using CarProject.Models;
+﻿using CarProject.Data;
+using CarProject.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CarProject.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
+        private readonly CarProjectContext _context; // database context
 
         [TempData]
         public string ErrorMessage { get; set; }
@@ -14,12 +19,16 @@ namespace CarProject.Pages {
         [BindProperty]
         public Inquiry Inquiry { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger) {
+        public IndexModel(ILogger<IndexModel> logger, CarProjectContext context) {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet() {
+        public IList<UserReview> UserReview { get; set; }
 
+        public async Task OnGetAsync() {
+            UserReview = await _context.UserReview
+                .Include(u => u.User).ToListAsync();
         }
 
         public IActionResult OnPost() {
